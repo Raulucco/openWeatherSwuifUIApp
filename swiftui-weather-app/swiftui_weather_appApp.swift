73aa22e7fa20fa794379572ce1f79d72adf27fd2
Raúl Calvo Martín <rauluccocm@gmail.com>
2024-12-15 20:03:04 +0100

    Initial Commit

---
+++ swiftui-weather-app/swiftui_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_weather_appApp.swift
+//  swiftui-weather-app
+//
+//  Created by Raul CM on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftui-weather-appTests/swiftui_weather_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_weather_appTests.swift
+//  swiftui-weather-appTests
+//
+//  Created by Raul CM on 15.12.2024.
+//
+
+import Testing
+@testable import swiftui_weather_app
+
+struct swiftui_weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
